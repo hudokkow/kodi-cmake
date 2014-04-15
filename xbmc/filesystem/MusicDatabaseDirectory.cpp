1b38bf63465bf29e45fe92b1f972e437ad3aaad0
Arne Morten Kvarving <cptspiff@gmail.com>
2014-04-15 23:58:34 +0200

    added: simple audiobooks node in music library
    no way to scan items to library yet. simply play an audio book and
    it will be added to the library

--- xbmc/filesystem/MusicDatabaseDirectory.cpp
+++ xbmc/filesystem/MusicDatabaseDirectory.cpp
@@ -221,6 +221,7 @@ bool CMusicDatabaseDirectory::GetLabel(const std::string& strDirectory, std::str
       strLabel = g_localizeStrings.Get(521);
       break;
     case NODE_TYPE_OVERVIEW:
+    case NODE_TYPE_AUDIOBOOKS:
       strLabel = "";
       break;
     default:
@@ -242,6 +243,7 @@ bool CMusicDatabaseDirectory::ContainsSongs(const std::string &path)
   if (type == MUSICDATABASEDIRECTORY::NODE_TYPE_ALBUM_TOP100_SONGS) return true;
   if (type == MUSICDATABASEDIRECTORY::NODE_TYPE_SONG_TOP100) return true;
   if (type == MUSICDATABASEDIRECTORY::NODE_TYPE_YEAR_SONG) return true;
+  if (type == MUSICDATABASEDIRECTORY::NODE_TYPE_AUDIOBOOKS) return true;
   return false;
 }
 
@@ -301,6 +303,8 @@ std::string CMusicDatabaseDirectory::GetIcon(const std::string &strDirectory)
     return "DefaultMusicYears.png";
   case NODE_TYPE_ALBUM_COMPILATIONS:
     return "DefaultMusicCompilations.png";
+  case NODE_TYPE_AUDIOBOOKS:
+    return "DefaultAudioBooks.png";
   default:
     break;
   }
