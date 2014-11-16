566f984b730119d40c5842770a54358e7c3847ff
Arne Morten Kvarving <cptspiff@gmail.com>
2014-11-16 14:54:10 +0100

    added: multiselection support to python dialog.select

--- xbmc/interfaces/legacy/Dialog.cpp
+++ xbmc/interfaces/legacy/Dialog.cpp
@@ -35,6 +35,7 @@
 #include "WindowException.h"
 #include "messaging/ApplicationMessenger.h"
 #include "Dialog.h"
+#include "FileItem.h"
 #ifdef TARGET_POSIX
 #include "linux/XTimeUtils.h"
 #endif
@@ -84,7 +85,7 @@ namespace XBMCAddon
       return pDialog->IsConfirmed();
     }
 
-    int Dialog::select(const String& heading, const std::vector<String>& list, int autoclose)
+    Alternative<int, std::vector<String> > Dialog::select(const String& heading, const std::vector<String>& list, int autoclose, bool multi)
     {
       DelayedCallGuard dcguard(languageHook);
       CGUIDialogSelect* pDialog= (CGUIDialogSelect*)g_windowManager.GetWindow(WINDOW_DIALOG_SELECT);
@@ -99,14 +100,26 @@ namespace XBMCAddon
       for(unsigned int i = 0; i < list.size(); i++)
       {
         listLine = list[i];
-          pDialog->Add(listLine);
+        pDialog->Add(listLine);
       }
       if (autoclose > 0)
         pDialog->SetAutoClose(autoclose);
 
-      pDialog->Open();
+      pDialog->SetMultiSelection(multi);
 
-      return pDialog->GetSelectedLabel();
+      //send message and wait for user input
+      XBMCWaitForThreadMessage(TMSG_DIALOG_DOMODAL, window, ACTIVE_WINDOW);
+      Alternative<int, std::vector<String> > ret;
+      if (multi)
+      {
+        const CFileItemList& items = pDialog->GetSelectedItems();
+        for (int i=0;i<items.Size();++i)
+          ret.later().push_back(items[i]->GetLabel());
+      }
+      else
+        ret.former() = pDialog->GetSelectedLabel();
+
+      return ret;
     }
 
     bool Dialog::ok(const String& heading, const String& line1, 
