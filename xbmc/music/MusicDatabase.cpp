77cccb9061655eb6ac5aa4875745414ac010fc24
Arne Morten Kvarving <cptspiff@gmail.com>
2015-04-10 13:49:44 +0200

    added: announce after audio library export

--- xbmc/music/MusicDatabase.cpp
+++ xbmc/music/MusicDatabase.cpp
@@ -4815,6 +4815,17 @@ void CMusicDatabase::ExportToXML(const std::string &xmlFile, bool singleFiles, b
     }
 
     xmlDoc.SaveFile(xmlFile);
+
+    if (singleFiles)
+      ANNOUNCEMENT::CAnnouncementManager::Get().Announce(ANNOUNCEMENT::AudioLibrary, "xbmc", "OnExport");
+    else
+    {
+      CVariant data;
+      data["xml"] = xmlFile;
+      if (iFailCount > 0)
+        data["failcount"] = iFailCount;
+      ANNOUNCEMENT::CAnnouncementManager::Get().Announce(ANNOUNCEMENT::AudioLibrary, "xbmc", "OnExport", data);
+    }
   }
   catch (...)
   {
